296d7463ca2521d749f5b610fd12b03c1acc9f07
LinoRino <omurakenshin@gmal.com>
2023-06-28 11:54:18 +0900

    add articles controller

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +19,6 @@ module InstagramClone
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_mailer.default_url_options = { host: ENV['APP_DEFAULT_URL_HOST'], port: ENV['APP_DEFAULT_URL_PORT'] }
   end
 end
