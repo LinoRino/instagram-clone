91569fb2d3d34dc239741f5bf8fe9415ad7af36e
LinoRino <omurakenshin@gmal.com>
2023-06-28 12:07:18 +0900

    fix model definition

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_015116) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
