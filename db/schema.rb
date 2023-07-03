ef43e606c22af8e8879d861fa6c53fbe71fca7d9
LinoRino <omurakenshin@gmal.com>
2023-07-03 11:55:34 +0900

    add posts & its comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_015116) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_024036) do
   create_table "active_storage_attachments", charset: "latin1", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_015116) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", charset: "latin1", force: :cascade do |t|
+    t.string "content"
+    t.bigint "users_id", null: false
+    t.bigint "posts_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
+  end
+
+  create_table "posts", charset: "latin1", force: :cascade do |t|
+    t.string "title"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
+
   create_table "users", charset: "latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_015116) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "posts", "users", column: "users_id"
 end
