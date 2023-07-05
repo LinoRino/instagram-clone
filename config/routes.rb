1f8c4df0cc0a87c94169141751774c5db64c3bb1
LinoRino <omurakenshin@gmal.com>
2023-07-05 12:13:47 +0900

    add path prefix config

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :articles, only: %i[index new]
+  resources :articles, only: %i[index new], path: ""
 end
