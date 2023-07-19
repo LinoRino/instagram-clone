d5e67cf7a04b77b89c8b3b476fd763c8bdbdf2da
LinoRino <omurakenshin@gmal.com>
2023-07-19 12:14:17 +0900

    Add update action to account controller.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,4 +2,20 @@ class AccountController < ApplicationController
   def edit
     @user = User.find(current_user.id)
   end
+
+  def update
+    if current_user.update(update_params)
+      flash[:success] = 'User was successfully updated'
+      render 'home'
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:post)
+  end
 end
