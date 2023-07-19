292766b87b42cbfd626d4851aefd12de51e300c0
LinoRino <omurakenshin@gmal.com>
2023-07-19 12:05:38 +0900

    Added more info to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_023408) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_024436) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_023408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "bio"
+    t.string "name"
+    t.string "gender"
+    t.string "website"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
