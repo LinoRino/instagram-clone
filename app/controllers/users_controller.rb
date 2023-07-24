e9771a2d2846f626522faab26ad29baf2c0213ca
LinoRino <omurakenshin@gmal.com>
2023-07-24 12:24:34 +0900

    add show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: params[:id])
+  end
+
+
   def update
     if User.update(update_params)
       flash[:success] = 'User was successfully updated'
-      render ''
+      render 'show'
     else
       flash[:error] = 'Something went wrong'
       render 'edit'
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   resources :home, only: %i[index], path: ""
   resources :posts, only: %i[index show new create]
-  resources :users, only: %i[index edit update]
+  resources :users, only: %i[index show edit update]
 end
