f8245c0c694b5078329c7283d2920463cc861ffc
LinoRino <omurakenshin@gmal.com>
2023-09-11 11:13:23 +0900

    add logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :home, only: %i[index], path: ""
+  resources :home, only: %i[index], path: ''
   resources :posts, only: %i[index show new create] do
     resource :likes, only: %i[create destroy]
   end
-  resources :users, only: %i[index show edit update]
+  resources :users, only: %i[index show edit update] do
+    resources :followers, only: %i[create destroy]
+  end
 end
