081c270710fec4656645cd4e63703876c14190ce
LinoRino <omurakenshin@gmal.com>
2023-09-25 11:55:12 +0900

    fix schema

--- db/migrate/20230909054028_create_followers.rb
+++ db/migrate/20230909054028_create_followers.rb
@@ -1,11 +1,10 @@
 class CreateFollowers < ActiveRecord::Migration[7.0]
   def change
     create_table :followers do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false, foreign_key: { to_table: :users }
+      t.integer :followed_id, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
-    add_index :followers, %i[follower_id followed_id], unique: true
   end
 end
