fdbb598630237db89415ef4a0b0f9575310643e9
LinoRino <omurakenshin@gmal.com>
2023-09-25 12:19:36 +0900

    add create & index property

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    input = comment_params.merge(user_id: current_user.id, post_id: params[:post_id])
+    @comment = Comment.new(input)
+    @comment.save
+    redirect_back(fallback_location: home_index_path)
+  end
+
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments, dependent: :destroy
 
   def liked_by?(user)
     Like.where(user_id: user.id, post_id: self.id).exists?
